c543a39e34342d6253e0461389f59377bdfba1da
Velid Heldic <velid2004@gmail.com>
2025-07-06 21:07:27 +0200

    Fixed logging error

--- UI/App.xaml.cs
+++ UI/App.xaml.cs
@@ -10,21 +10,34 @@ using System.Windows;
 using UI.Converters;
 using static UI.ViewModels.TourManagementViewModel;
 
-namespace SWEN_TourPlanner
+namespace UI
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(App));
+
         protected override void OnStartup(StartupEventArgs e)
         {
-            base.OnStartup(e);
+            string log4netConfigFilename = "log4net.config";
+            if (!File.Exists(log4netConfigFilename))
+            {
+                MessageBox.Show("log4net.config not found. Logging won't work.");
+            }
+
+            // log4net.config gets copied next to exe file using Build Action
 
             var logRepository = LogManager.GetRepository(System.Reflection.Assembly.GetEntryAssembly());
-            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+            XmlConfigurator.Configure(logRepository, new FileInfo(log4netConfigFilename));
 
+            log.Debug("Debug log test");
+            log.Info("Info log test");
+            log.Warn("Warning log test");
+            log.Error("Error log test");
             
+            base.OnStartup(e);
         }
     }
 
